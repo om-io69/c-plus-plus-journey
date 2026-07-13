c87ed971f6a056cb4cd40c449889e1a07d761e92
om-io69 <nalawadeom811@gmail.com>
2026-07-14 00:03:35 +0530

    Add files via upload

---
+++ tut7.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int age;
+    cout<<"tell me your age"<<endl;
+    cin>>age;
+
+    switch (age)
+    {
+    case 18:
+        /* code */
+        cout<<"you are 18";
+        break;
+    
+
+    }
+
+    return 0;
+    }
