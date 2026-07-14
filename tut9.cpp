51e53eeb45468f3f38e98b1a85ea88bc2df61dd1
om-io69 <nalawadeom811@gmail.com>
2026-07-14 19:54:38 +0530

    Add files via upload

---
+++ tut9.cpp
@@ -0,0 +1,29 @@
+//for loop
+#include <iostream>
+
+using namespace std;
+int main(){
+/*for (int i=1;i<=30;i++)
+{  cout<<i<<endl;
+}
+
+*/
+/*int i=1 ;
+while (i<=40)
+{
+    /* code */
+   // cout<<i<<endl;
+   // i++;
+int i=1;
+   do
+   {
+    /* code */
+    cout<<i<<endl;
+    i++;
+   } while (i<40);
+   
+
+
+
+return 0;
+}
